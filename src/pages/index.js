1a1b51c3576af26b7800054fcc998fb2e12d396a
asadk-mecha <asadk@mechasystems.com>
2025-01-07 09:27:32 +0530

    feat(ui): update ui changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '@theme/Layout';
 export default function Hello() {
   return (
-  <Layout title="Hello" description="Hello React Page">
+  <Layout >
   <div
     style={{
       display: 'flex',
@@ -22,7 +22,7 @@ export default function Hello() {
  
     
     <span  className="landing-page-heading">Work in Progress</span>
-    <span className="landing-page-description">We are working on releasing our documentation for the Mecha Comet and our Cloud Services, meanwhile you can review the WIP docs here and on our code repository at Github.</span >
+    <span className="landing-page-description">We are working on releasing our documentation for the Mecha Comet and our Cloud Services, meanwhile you can review the WIP docs on our code repository <a target="_blank" style={{color:"blue"}} href="https://github.com/mecha-org/mecha-docs">here</a>.</span >
   </div>
 </Layout>
   );
