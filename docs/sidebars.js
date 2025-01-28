13ba0b093c1df6203ef655da5cfa408b2fc0bf35
dhruveshb-mecha <dhruveshb@mechasystems.com>
2025-01-28 01:15:22 -0500

    docs: add guides for adding source

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -142,6 +142,7 @@ const sidebars = {
         "mechanix/installing-apps/package-manager",
         "mechanix/installing-apps/using-flatpak",
         "mechanix/installing-apps/using-snap",
+        "mechanix/installing-apps/adding-source",
       ],
     },
   ],
