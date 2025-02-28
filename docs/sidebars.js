739a4467e11ee1381605f1a252ad898ee60ddac6
mineshp <mineshp@mechasystems.com>
2025-02-28 18:51:37 +0530

    feat(comet): adds emulations doc

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -97,6 +97,7 @@ const sidebars = {
         "comet/emulation/windows",
         "comet/emulation/linux",
         "comet/emulation/dosbox",
+        "comet/emulation/waydroid",
       ],
     },
   ],
