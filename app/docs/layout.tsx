155633121292028ade8eaeed5cbecacb03ba3087
asyncasad <asyncasad@gmail.com>
2025-12-21 21:28:52 +0530

    feat(init): initialise project

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,10 +1,23 @@
-import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { baseOptions } from '@/lib/layout.shared';
+import { source } from "@/lib/source";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { baseOptions } from "@/lib/layout.shared";
+import Logo from "../../public/logo.png";
+import Image from "next/image";
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
+export default function Layout({ children }: LayoutProps<"/docs">) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout
+      tree={source.pageTree}
+      {...baseOptions()}
+      nav={{
+        title: (
+          <>
+            <Image src={Logo} alt="mecha-log" className="w-6 h-6" width={24} height={24}/>
+            <span className="font-medium">Mecha Docs</span>
+          </>
+        ),
+      }}
+    >
       {children}
     </DocsLayout>
   );
